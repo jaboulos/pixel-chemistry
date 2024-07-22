ccf94387e1e39ca1c4919c4a275b5f33e49821e2
Joseph Boulos <jab12288@gmail.com>
2024-07-22 15:47:37 -0700

    prisma initial setup, setup some tables in prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,10 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 import authConfig from './auth.config'
+import { prisma } from './lib/prisma'
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 export const {
   auth,
