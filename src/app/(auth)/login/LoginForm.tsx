df2981fe4a81590b654940a73ce39e77e1944e98
Joseph Boulos <jab12288@gmail.com>
2024-07-22 09:25:22 -0700

    add initial form and form validation with react-hook-form on login page, zod validation incoming

--- src/app/(auth)/login/LoginForm.tsx
+++ src/app/(auth)/login/LoginForm.tsx
@@ -1,8 +1,21 @@
+'use client'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
 import { GiPadlock } from 'react-icons/gi'
 
 export const LoginForm = () => {
+  // hook from react-hook-form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm()
+
+  const onSubmit = (data: any) => {
+    console.log('data', data)
+  }
+
   return (
     <>
       <Card className="w/25 mx-auto">
@@ -16,11 +29,33 @@ export const LoginForm = () => {
           </div>
         </CardHeader>
         <CardBody>
-          <form action="">
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="space-y-4">
-              <Input label="Email" variant="bordered" />
-              <Input label="Password" type="password" variant="bordered" />
-              <Button fullWidth color="secondary" type="submit">
+              <Input
+                // {...register('email', { required: true })} // this syntax for no error message and only highlight
+                {...register('email', { required: 'Email is required' })}
+                defaultValue=""
+                label="Email"
+                variant="bordered"
+                isInvalid={!!errors.email}
+                errorMessage={errors.email?.message as string}
+              />
+              <Input
+                // {...register('password', { required: true })} // this syntax for no error message and only highlight
+                {...register('password', { required: 'Password is required' })}
+                defaultValue=""
+                label="Password"
+                type="password"
+                variant="bordered"
+                isInvalid={!!errors.password}
+                errorMessage={errors.password?.message as string}
+              />
+              <Button
+                fullWidth
+                isDisabled={!isValid}
+                color="secondary"
+                type="submit"
+              >
                 Login
               </Button>
             </div>
