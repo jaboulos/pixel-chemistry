d3edff3ad72dfe12477d036992b364f94ffe3dde
Joseph Boulos <jab12288@gmail.com>
2024-07-23 11:23:50 -0700

    add type definitions file, update authAction registerUser to return response that matches the type defintion authAction assigned to it

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -6,45 +6,62 @@
 import bcrypt from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { registerSchema, RegisterSchema } from '@/lib/schemas/registerSchema'
+import { User } from '@prisma/client'
+import { ActionResult } from '@/types'
 
 // take in form fields from forms, validation and process for registering user
-export const registerUser = async (data: RegisterSchema) => {
-  // validate on server side as well as client side, easy to do with zod
-  // data is validated on server side
-  // in case this function gets called outside of the server somehow, still needs to pass this validation
-  const validated = registerSchema.safeParse(data)
+export const registerUser = async (
+  data: RegisterSchema,
+  // return promise of type ActionResult
+  // return a User from this method
+): Promise<ActionResult<User>> => {
+  try {
+    // validate on server side as well as client side, easy to do with zod
+    // data is validated on server side
+    // in case this function gets called outside of the server somehow, still needs to pass this validation
+    const validated = registerSchema.safeParse(data)
 
-  // if its not valid, throw error
-  if (!validated.success) {
-    return { error: validated?.error?.errors }
-  }
+    // if its not valid, throw error
+    if (!validated.success) {
+      // add the property "status" because it is part of the type defintion of what can be returned by ActionResult
+      return { status: 'error', error: validated?.error?.errors }
+    }
 
-  // if data passes validation, start process of creating user in db
-  const { name, email, password } = validated.data
+    // if data passes validation, start process of creating user in db
+    const { name, email, password } = validated.data
 
-  // hash the password with bcrypt
-  const hashedPassword = await bcrypt.hash(password, 10)
+    // hash the password with bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-  // check if user already exists in db with existing email
-  const existingUser = await prisma.user.findUnique({
-    where: { email: email },
-  })
+    // check if user already exists in db with existing email
+    const existingUser = await prisma.user.findUnique({
+      where: { email: email },
+    })
 
-  // if user already exists throw server side error
-  if (existingUser) {
+    // if user already exists throw server side error
+    if (existingUser) {
+      return {
+        status: 'error',
+        error: 'User already exists',
+      }
+    }
+
+    // otherwise create user in db
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash: hashedPassword,
+      },
+    })
+
+    // return in format typescript is expecting of type ActionResult
+    return { status: 'success', data: user }
+  } catch (error) {
+    console.log(error)
     return {
-      error: 'User already exists',
+      status: 'error',
+      error: 'Something went wrong',
     }
   }
-
-  // otherwise create user in db
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      passwordHash: hashedPassword,
-    },
-  })
-
-  return user
 }
