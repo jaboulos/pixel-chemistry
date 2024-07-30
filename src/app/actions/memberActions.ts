e7ec07068855749ff05a6515f8e3d7c92c19ff67
Joseph Boulos <jab12288@gmail.com>
2024-07-30 11:45:00 -0700

    add generic error page, update some styles

--- src/app/actions/memberActions.ts
+++ src/app/actions/memberActions.ts
@@ -7,7 +7,6 @@ import { Photo } from '@prisma/client'
 // querying local db to get data
 export const getMembers = async () => {
   // use this anywhere to get session data
-  throw new Error('testing...')
   const session = await auth()
 
   if (!session?.user) return null
