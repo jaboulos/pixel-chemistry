59685b7ba85fd8cae281e16616089bed171d85aa
Joseph Boulos <jab12288@gmail.com>
2024-07-30 09:18:23 -0700

    add loading indicator

--- src/app/members/[userId]/photos/page.tsx
+++ src/app/members/[userId]/photos/page.tsx
@@ -10,7 +10,7 @@ export const PhotosPage = async ({
   const photos = await getMemberPhotosByUserId(params.userId)
   return (
     <>
-      {/* <CardHeader className="text-2xl font-semibold text-secondary">
+      <CardHeader className="text-2xl font-semibold text-secondary">
         Profile
       </CardHeader>
       <Divider />
@@ -25,27 +25,6 @@ export const PhotosPage = async ({
                   className="object-cover aspect-square"
                   src={p.url}
                   alt="member image"
-                />
-              </div>
-            ))}
-        </div>
-      </CardBody> */}
-
-      <CardHeader className="text-2xl font-semibold text-secondary">
-        Photos
-      </CardHeader>
-      <Divider />
-      <CardBody>
-        <div className="grid grid-cols-5 gap-3">
-          {photos &&
-            photos.map((photo) => (
-              <div key={photo.id}>
-                <Image
-                  width={300}
-                  height={300}
-                  src={photo.url}
-                  alt="Image of member"
-                  className="object-cover aspect-square"
                 />
               </div>
             ))}
