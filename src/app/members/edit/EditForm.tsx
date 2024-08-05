b0340aee57b5dd102ce0a53b864539382e6452ea
Joseph Boulos <jab12288@gmail.com>
2024-08-05 14:39:59 -0700

    add memberEditSchema and EditForm component

--- src/app/members/edit/EditForm.tsx
+++ src/app/members/edit/EditForm.tsx
@@ -1,11 +1,24 @@
+'use client'
+import { memberEditSchema } from '@/lib/schemas/memberEditSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Member } from '@prisma/client'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 type EditFormProps = {
   member: Member
 }
 
 export const EditForm = ({ member }: EditFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid, isDirty, isSubmitting },
+  } = useForm({
+    resolver: zodResolver(memberEditSchema),
+  })
+
   return (
     <div>
       <div>edit form</div>
