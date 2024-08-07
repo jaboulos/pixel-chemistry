63b0c0fc8913e85d9b2e3c64d4221cce9e064d53
Joseph Boulos <jab12288@gmail.com>
2024-08-07 09:49:58 -0700

    resolve cloudinary upload issues

--- src/app/actions/userActions.ts
+++ src/app/actions/userActions.ts
@@ -15,87 +15,49 @@ import { Member } from '@prisma/client'
  * @param {MemberEditSchemaType} data - The new data for the member's profile.
  * @returns {Promise<ActionResult<Member>>} - The result of the update action.
  */
-// export const updateMemberProfile = async (
-//   data: MemberEditSchemaType,
-// ): Promise<ActionResult<Member>> => {
-//   try {
-//     // Get the authenticated user's ID
-//     const userId = await getAuthUserId()
-
-//     // Validate the input data using the member edit schema
-//     const validated = memberEditSchema.safeParse(data)
-
-//     // If validation fails, return an error with validation errors
-//     if (!validated.success) {
-//       return {
-//         status: 'error',
-//         error: validated.error.errors,
-//       }
-//     }
-
-//     // Destructure the validated data
-//     const { name, description, city, country } = validated.data
-
-//     // Update the member's profile in the database using Prisma
-//     const member = await prisma.member.update({
-//       where: { userId }, // Find the member by user ID
-//       data: {
-//         // Update the member's profile with the new data
-//         name,
-//         description,
-//         city,
-//         country,
-//       },
-//     })
-
-//     // Return a success response with the updated member data
-//     return {
-//       status: 'success',
-//       data: member,
-//     }
-//   } catch (error) {
-//     // Log the error to the console
-//     console.log(error)
-
-//     // Return a generic error response
-//     return { status: 'error', error: 'Something went wrong' }
-//   }
-// }
-
-export async function updateMemberProfile(
+export const updateMemberProfile = async (
   data: MemberEditSchemaType,
-  nameUpdated: boolean,
-): Promise<ActionResult<Member>> {
+): Promise<ActionResult<Member>> => {
   try {
+    // Get the authenticated user's ID
     const userId = await getAuthUserId()
 
+    // Validate the input data using the member edit schema
     const validated = memberEditSchema.safeParse(data)
 
-    if (!validated.success)
-      return { status: 'error', error: validated.error.errors }
-
-    const { name, description, city, country } = validated.data
-
-    if (nameUpdated) {
-      await prisma.user.update({
-        where: { id: userId },
-        data: { name },
-      })
+    // If validation fails, return an error with validation errors
+    if (!validated.success) {
+      return {
+        status: 'error',
+        error: validated.error.errors,
+      }
     }
 
+    // Destructure the validated data
+    const { name, description, city, country } = validated.data
+
+    // Update the member's profile in the database using Prisma
     const member = await prisma.member.update({
-      where: { userId },
+      where: { userId }, // Find the member by user ID
       data: {
+        // Update the member's profile with the new data
         name,
         description,
         city,
         country,
       },
     })
-    return { status: 'success', data: member }
+
+    // Return a success response with the updated member data
+    return {
+      status: 'success',
+      data: member,
+    }
   } catch (error) {
+    // Log the error to the console
     console.log(error)
 
+    // Return a generic error response
     return { status: 'error', error: 'Something went wrong' }
   }
 }
--- src/app/members/edit/layout.tsx
+++ src/app/members/edit/layout.tsx
@@ -1,84 +1,18 @@
-// // import { getMemberByUserId } from '@/app/actions/memberActions'
-// // import { notFound } from 'next/navigation'
-// // import React, { ReactNode } from 'react'
-// // import MemberSidebar from '../MemberSidebar'
-// // import { Card } from '@nextui-org/react'
-// // import { getAuthUserId } from '@/app/actions/authActions'
-
-// // export const Layout = async ({ children }: { children: ReactNode }) => {
-// //   const userId = await getAuthUserId()
-// //   // optimization issue, calls in layout and page are duplicated.  According to next docs, this is normal
-// //   const member = await getMemberByUserId(userId)
-
-// //   if (!member) {
-// //     return notFound()
-// //   }
-
-// //   const basePath = `/members/edit`
-
-// //   const navLinks = [
-// //     { name: 'Edit Profile', href: `${basePath}` },
-// //     { name: 'Update Photos', href: `${basePath}/photos` },
-// //   ]
-
-// //   return (
-// //     <div className="grid grid-cols-12 gap-5 h-[80vh]">
-// //       <div className="col-span-3">
-// //         <MemberSidebar member={member} navLinks={navLinks} />
-// //       </div>
-// //       <div className="col-span-9">
-// //         <Card className="w-full mt-10 h-[80vh]">{children}</Card>
-// //       </div>
-// //     </div>
-// //   )
-// // }
-
-// // export default Layout
-
-// import { getMemberByUserId } from '@/app/actions/memberActions'
-// import React, { ReactNode } from 'react'
-// import MemberSidebar from '../MemberSidebar'
-// import { notFound } from 'next/navigation'
-// import { Card } from '@nextui-org/react'
-// import { getAuthUserId } from '@/app/actions/authActions'
-
-// export default async function Layout({ children }: { children: ReactNode }) {
-//   const userId = await getAuthUserId()
-
-//   const member = await getMemberByUserId(userId)
-//   if (!member) return notFound()
-
-//   const basePath = `/members/edit`
-
-//   const navLinks = [
-//     { name: 'Edit Profile', href: `${basePath}` },
-//     { name: 'Update Photos', href: `${basePath}/photos` },
-//   ]
-
-//   return (
-//     <div className="grid grid-cols-12 gap-5 h-[80vh]">
-//       <div className="col-span-3">
-//         <MemberSidebar member={member} navLinks={navLinks} />
-//       </div>
-//       <div className="col-span-9">
-//         <Card className="w-full mt-10 h-[80vh]">{children}</Card>
-//       </div>
-//     </div>
-//   )
-// }
-
 import { getMemberByUserId } from '@/app/actions/memberActions'
+import { notFound } from 'next/navigation'
 import React, { ReactNode } from 'react'
 import MemberSidebar from '../MemberSidebar'
-import { notFound } from 'next/navigation'
 import { Card } from '@nextui-org/react'
 import { getAuthUserId } from '@/app/actions/authActions'
 
-export default async function Layout({ children }: { children: ReactNode }) {
+export const Layout = async ({ children }: { children: ReactNode }) => {
   const userId = await getAuthUserId()
-
+  // optimization issue, calls in layout and page are duplicated.  According to next docs, this is normal
   const member = await getMemberByUserId(userId)
-  if (!member) return notFound()
+
+  if (!member) {
+    return notFound()
+  }
 
   const basePath = `/members/edit`
 
@@ -98,3 +32,5 @@ export default async function Layout({ children }: { children: ReactNode }) {
     </div>
   )
 }
+
+export default Layout
