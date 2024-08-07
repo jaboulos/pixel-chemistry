309bbe74a6cc61c38da040eaa87a058c6178717f
Joseph Boulos <jab12288@gmail.com>
2024-08-07 11:55:34 -0700

    update MemberImage component

--- src/components/MemberImage.tsx
+++ src/components/MemberImage.tsx
@@ -1,73 +1,36 @@
-// 'use client'
-// import { Photo } from '@prisma/client'
-// import { CldImage } from 'next-cloudinary'
-// import React from 'react'
-// import { Image } from '@nextui-org/react'
-
-// type MemberImageProps = {
-//   photo: Photo | null
-// }
-
-// export const MemberImage = ({ photo }: MemberImageProps) => {
-//   return (
-//     <div>
-//       {photo?.publicId ? (
-//         <CldImage
-//           alt="member image"
-//           src={photo.publicId}
-//           width={300}
-//           height={300}
-//           crop="fill"
-//           gravity="faces"
-//           className="rounded-2xl"
-//         />
-//       ) : (
-//         <Image
-//           width={220}
-//           height={220}
-//           src={photo?.url || '/images/user.png'}
-//           alt="member image"
-//         />
-//       )}
-//     </div>
-//   )
-// }
-
-// export default MemberImage
-
 'use client'
-
 import { Photo } from '@prisma/client'
 import { CldImage } from 'next-cloudinary'
 import React from 'react'
 import { Image } from '@nextui-org/react'
 
-type Props = {
+type MemberImageProps = {
   photo: Photo | null
 }
 
-export default function MemberImage({ photo }: Props) {
+export const MemberImage = ({ photo }: MemberImageProps) => {
   return (
     <div>
       {photo?.publicId ? (
         <CldImage
-          alt="Image of member"
+          alt="member image"
           src={photo.publicId}
           width={300}
           height={300}
           crop="fill"
           gravity="faces"
           className="rounded-2xl"
-          priority
         />
       ) : (
         <Image
           width={220}
           height={220}
           src={photo?.url || '/images/user.png'}
-          alt="Image of user"
+          alt="member image"
         />
       )}
     </div>
   )
 }
+
+export default MemberImage
