de4b41b22229235547e1260149b1386abfc2d519
Joseph Boulos <jab12288@gmail.com>
2024-08-15 16:23:32 -0700

    update imageUploadButton style, rearange edit profile card header components

--- src/app/members/edit/photos/MemberPhotoUpload.tsx
+++ src/app/members/edit/photos/MemberPhotoUpload.tsx
@@ -19,7 +19,7 @@ export const MemberPhotoUpload = () => {
   }
 
   return (
-    <div className="pt-5 pl-5">
+    <div>
       <ImageUploadButton onUploadImage={onAddImage} />
     </div>
   )
--- src/app/members/edit/photos/page.tsx
+++ src/app/members/edit/photos/page.tsx
@@ -23,12 +23,14 @@ export const PhotosPage = async () => {
 
   return (
     <>
-      <CardHeader className="text-2xl font-semibold text-secondary">
-        Edit Profile
+      <CardHeader className="flex flex-row justify-between items-center">
+        <div className="text-2xl font-semibold text-secondary">
+          Edit Profile
+        </div>
+        <MemberPhotoUpload />
       </CardHeader>
       <Divider />
       <CardBody>
-        <MemberPhotoUpload />
         <MemberPhotos
           photos={photos}
           editing={true}
