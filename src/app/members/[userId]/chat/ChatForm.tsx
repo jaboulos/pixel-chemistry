b298fb93b900d1249baa5ce1e553c14bb30b91a0
Joseph Boulos <jab12288@gmail.com>
2024-08-29 10:05:04 -0700

    setup chat form

--- src/app/members/[userId]/chat/ChatForm.tsx
+++ src/app/members/[userId]/chat/ChatForm.tsx
@@ -1,6 +1,10 @@
-import { MessageSchema } from '@/lib/schemas/messageSchema'
+'use client'
+import { messageSchema, MessageSchema } from '@/lib/schemas/messageSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Input } from '@nextui-org/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { HiPaperAirplane } from 'react-icons/hi2'
 
 export const ChatForm = () => {
   const {
@@ -8,9 +12,39 @@ export const ChatForm = () => {
     handleSubmit,
     reset,
     formState: { isSubmitting, isValid, errors },
-  } = useForm<MessageSchema>()
+  } = useForm<MessageSchema>({
+    resolver: zodResolver(messageSchema),
+  })
 
-  return <div>ChatForm</div>
+  const onSubmit = (data: MessageSchema) => {
+    console.log(data)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full flex items-center gap-2"
+    >
+      <Input
+        fullWidth
+        placeholder="Type a message"
+        variant="faded"
+        {...register('text')}
+        isInvalid={!!errors.text}
+        errorMessage={errors.text?.message}
+      />
+      <Button
+        type="submit"
+        isIconOnly
+        color="secondary"
+        radius="full"
+        isLoading={isSubmitting}
+        isDisabled={!isValid || isSubmitting}
+      >
+        <HiPaperAirplane size={18} />
+      </Button>
+    </form>
+  )
 }
 
 export default ChatForm
